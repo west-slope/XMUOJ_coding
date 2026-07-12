98ddb25da5e578e4030bf1d84207808da572ad53
whale <280937040+west-slope@users.noreply.github.com>
2026-07-12 22:58:39 +0800

    update

--- solutions/359/053.cpp
+++ solutions/359/053.cpp
@@ -2,19 +2,7 @@
 #include <cmath>
 using namespace std;
 typedef long long LL;
-// bool isPrime(LL n)
-// {
-//     if(n<2)return false;
-//     else if(n==2)return true;
-//     else if(n%2==0)return false;
-//     double m=sqrt(n);
-//     for(int i=3;i<=m;i+=2)
-//     {
-//         if(n%i==0)return false;
-//     }
-//     return true;
 
-// }
 #include <iostream>
 using namespace std;
 typedef long long LL;
@@ -34,28 +22,3 @@ int main() {
     return 0;
 }
 //主包过不了，以后再看……
-
-
-//超出时长，无法通过……
-// int main() 
-// {
-//     LL N;
-//     cin >> N;
-//     for (LL i = 2; i <= N; i+=2) {
-//         if(isPrime(i))continue;
-//         LL sum = 1;
-//         LL m=sqrt(i);
-//         for (int j = 2; j <= m; j++) {
-//             if (i % j == 0) {
-//                 sum += j;   
-//                 if(j!=i/j)
-//                 sum+=i/j;
-//             }
-//         }
-//         if (sum == i) {
-//             cout << i << endl;
-//         }
-    
-//     }
-//     return 0;
-// }
