8887b1b9d4b397017f709e93458162c4fb00840a
whale <280937040+west-slope@users.noreply.github.com>
2026-07-13 08:00:48 +0800

    增加了公告功能，解决部分题目没用图片的问题，优化了python文件的自动读取，增加了多个题解的功能，优化了页面加载速度。

--- solutions/362/LinK02.cpp
+++ solutions/362/LinK02.cpp
@@ -6,21 +6,11 @@ int main()
 	int a, b, c, d, N;
 	cin >> N;
 	for (int i = 2; i <= N; i++)
-	{
 		for (int j = 2; j < N; j++)
-		{
 			for (int k = j; k < N; k++)
-			{
 				for (int l = k; l < N; l++)
-				{
 					if (i * i * i == j * j * j + k * k * k + l * l * l)
-					{
 						cout << "Cube = " << i << ", Triple = (" << j <<","<< k <<"," << l << ")" << endl;
-					}
-				}
-			}
-		}
-	}
 
 	return 0;
 }
